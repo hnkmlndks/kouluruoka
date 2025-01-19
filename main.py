83a56f99248f3c24e3e1d3777155ad42eb4cb9f5
hnkmlndks <qim@dds.nl>
2025-01-19 17:45:59 +0200

    full rewrite due to change on website

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ import logging
 
 forbidden_characters = ['&']
 
-logging.basicConfig(
-    filename="app.log",
-    filemode="a",
-    encoding='utf-8',
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    level=logging.INFO
-)
+TEST = False
+
+if TEST:
+    logging.basicConfig(
+        filename="app.log",
+        filemode="w",
+        encoding='utf-8',
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        level=logging.DEBUG
+    )
+else:
+    logging.basicConfig(
+        filename="app.log",
+        filemode="a",
+        encoding='utf-8',
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        level=logging.INFO
+    )
 
 # Function to remove forbidden characters using list comprehension
 def remove_forbidden_characters(input_string, forbidden_characters):
     return ''.join([char for char in input_string if char not in forbidden_characters])
-
-
 
 
 def send_telegram(message):
@@ -39,37 +48,76 @@ def send_telegram(message):
     else:
         logging.error("Failed to publish the message to instagram. Status code:", response.status_code)
 
+def clean_text(text):
+    return re.sub(r'\s*\([^)]*\)', '', text).strip()
+
+
+def process_string(input_string):
+    items = input_string.split('), ')
+    processed_items = []
+
+    for item in items:
+        main_part, _, parentheses_part = item.partition('(')
+        main_part = main_part.strip()
+
+        if 'Veg' in parentheses_part:
+            processed_items.append(f"{main_part} [V]")
+        else:
+            processed_items.append(main_part)
+
+    return ', '.join(processed_items)
+
 
 # URL of the kouluruoka menu page
 url = "https://kouluruoka.fi/menu/helsinki_kapylanperuskouluhykkyla/"
 # Send an HTTP GET request to the URL
 response = requests.get(url)
+logging.debug(f"response: {response}")
 # Check if the request was successful
 if response.status_code == 200:
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
     # Find all day menu items
-    day_menus = soup.find_all(class_="menu-module--dayMenu--eb50b")
+    logging.debug(f"soup: {soup}")
     school = soup.find('h1', id='pageTitle').get_text(strip=True)
+    logging.debug(f"school:{school}")
     #print(f"school: {school}")
     message = ""
     # Extract lunch options for each day
-    for day_menu in day_menus:
-        day_title = day_menu.find('h2').string.strip()
-        lunch_options = day_menu.find_all('h3', string=lambda t: t and 'Lounas' in t)
-        lunch = ""
-        #print(f"**{day_title}**")
-        for lunch_option in lunch_options:
-            lunch_details = lunch_option.find_next('div').get_text(strip=True)
+    articles = soup.find_all('article')
+    #print(articles)
 
-            lunch_details = lunch_details.replace("Veg", ")[V](")
-            lunch_details = re.sub(r'\(.*?\)', '', lunch_details)
-            #print(lunch_details)
-            lunch += f"**{lunch_details}\n"
-        message += f"{day_title}\n{lunch}\n"
+    for article in articles:
+        # Extract the day title from <h2> tag
+        day_title = article.find('h2').text
+
+        # Find all div elements that are direct children of the article
+        div_elements = soup.article.find_all('div', recursive=False)
+
+        # Extract the Lounas items and save them as items
+        items = []
+        for div in div_elements:
+            span = div.find('span')
+            if span:
+                items.append(span.text)
+
+        # Print the extracted items
+        lunch_options = ""
+        for item in items:
+            # print(item)
+            lunch_options += f"\n**{process_string(item)}"
+
+        # break
+
+        # Print the extracted values
+        #print("Day Title:", day_title)
+        #print("Lunch option:", lunch_options)
+
+        message += f"{day_title}{lunch_options}\n\n"
         message = remove_forbidden_characters(message, forbidden_characters)
 
     message = f"{school}\n{message}"
+    print(message)
     logging.info(f"Telegram message prepared for {school}")
 
     send_telegram(message)
